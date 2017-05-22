91a83921770436eceda179fb064b2054f0a5c2e7
mirkocoric <mirkocoric@gmail.com>
2017-05-22 14:33:20 +0200

    Fifth commit

--- firstTen.py
+++ firstTen.py
@@ -2,18 +2,17 @@ from __future__ import print_function
 from bs4 import BeautifulSoup
 import requests
 
+class Element:
+    def __init__(self, naslov, cijena):
+        self.naslov = naslov 
+        self.cijena = cijena
+
 def is_int(s):
     try:
         int(s)
         return True
     except ValueError:
-        return False
-
-class Element:
-    def __init__(self, naslov, cijena):
-        self.naslov = naslov 
-        self.cijena = cijena
-   
+        return False   
 
 def find_all_elements (elements, soup):  
     for li in soup.find_all('article'):
@@ -36,42 +35,50 @@ def find_all_elements (elements, soup):
                         elements.append(Element(naslov, cijena))
     return elements
 
+def findPageNumbers (home):
+    page = requests.get(home) 
+    soup = BeautifulSoup (page.content, 'lxml')
+    buttons = soup.find_all ('button')
+    numbers = [] 
+    for button in buttons:
+        if 'data-page' in button.attrs:
+            listNum = button.text.split("-")
+            for num in listNum:
+                numbers.append(num) 
+
+    pageNum = 0
+    for number in numbers:
+     if is_int(number):
+        num = int(number)
+        if num > pageNum:
+            pageNum = num
+    return pageNum
+    
+def printElements (elements):
+    for element in elements:
+    #print(vars(element))
+        print ('Naslov: ' + element.naslov, end = ' ')
+        if element.cijena:
+            print('Cijena: ', end = ' ') 
+            for cijena in element.cijena:
+                if cijena[-1:] == '~':               
+                    print (cijena[:-1], end = ' ') 
+                else:   
+                    print (cijena, end = ' ') 
+        print()
+
 elements = []
 home = 'http://www.njuskalo.hr/iznajmljivanje-poslovnih-prostora'
 url = 'http://www.njuskalo.hr/iznajmljivanje-poslovnih-prostora?page='
 
-page = requests.get(home) 
-soup = BeautifulSoup (page.content, 'lxml')
-buttons = soup.find_all ('button')
-numbers = [] 
-for button in buttons:
-    if 'data-page' in button.attrs:
-        listNum = button.text.split("-")
-        for num in listNum:
-            numbers.append(num) 
 
-max = 0
-for number in numbers:
-    if is_int(number):
-        num = int(number)
-        if num > max:
-            max = num
+pageNum = findPageNumbers (home)
 
-for i in xrange(max):
+for i in xrange(pageNum):
     newUrl = url + str(i+1)
     page = requests.get(newUrl)   
     soup = BeautifulSoup (page.content, 'lxml')
     elements = find_all_elements(elements, soup)
 
-for element in elements:
-    #print(vars(element))
-    print ('Naslov: ' + element.naslov, end = ' ')
-    if element.cijena:
-        print('Cijena: ', end = ' ') 
-        for cijena in element.cijena:
-            if cijena[-1:] == '~':               
-                print (cijena[:-1], end = ' ') 
-            else:   
-                print (cijena, end = ' ') 
-    print()
+printElements (elements)
 
